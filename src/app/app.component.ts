3366d90a19b57b23d0b60837ff72dbb3c55ae591
ThomasMignon <thomas.mignon51@laposte.com>
2017-08-01 11:35:55 +0200

    avancement pokedex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export class Pokemon {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
-  results = [];
 
+export class AppComponent implements OnInit {
+  title = 'Pokedex';
+
+  selectedPkm: Pokemon;
+
+  onSelect(pokemon: Pokemon): void {
+    this.selectedPkm = pokemon;
+  }
+
+  results = [];
+	
   constructor(private http: HttpClient){}
 
   ngOnInit(): void {
-    this.http.get('http://pokeapi.co/api/v2/pokemon/').subscribe(data => {
+    this.http.get('http://pokeapi.co/api/v2/pokemon/?limit=151&offset=0').subscribe(data => {
       this.results = data['results'];
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule }   from '@angular/router';
 
 @NgModule({
   declarations: [
